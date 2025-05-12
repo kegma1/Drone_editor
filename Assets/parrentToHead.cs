686025d6519b2c512606c08c2d0e3e5bc2a8f2cb
Kennet Mathisen <kennetam02@gmail.com>
2025-05-12 16:16:11 +0200

    broder æ blir å gråte

--- Assets/parrentToHead.cs
+++ Assets/parrentToHead.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class parrentToHead : MonoBehaviour
 {
@@ -6,6 +7,11 @@ public class parrentToHead : MonoBehaviour
     void Start()
     {
         var iglooCamera = GameObject.Find("IglooMainCamera");
+        var camera = iglooCamera.GetComponent<Camera>();
+        Debug.Log(camera);
+        var additionalData = camera.GetUniversalAdditionalCameraData();
+        additionalData.renderPostProcessing = true;
+        
         transform.parent = iglooCamera.transform;
     }
 }
