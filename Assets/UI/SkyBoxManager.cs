aa9749a9a0981f1c3e8f8e53ad61a454fee40207
Kennet Mathisen <kennetam02@gmail.com>
2025-05-15 17:26:23 +0200

    skybox fixed 😎😎😎

--- Assets/UI/SkyBoxManager.cs
+++ Assets/UI/SkyBoxManager.cs
@@ -1,16 +1,70 @@
+using System;
 using UnityEngine;
+using SimpleFileBrowser;
+using System.Collections;
+using System.IO;
 
 public class SkyBoxManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    public ErrorManager errorManager;
+    private Material skybox;
+    
+    public Texture2D OriginalSkybox;
+    
+    void Start() {
+        skybox = RenderSettings.skybox;
+        onOriginalBackground();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void onOriginalBackground() {
+            skybox.SetTexture("_MainTex", OriginalSkybox);
+            RenderSettings.skybox = skybox;
+
+            DynamicGI.UpdateEnvironment();
+    }
+
+    public void onChangeBackground() {
+        StartCoroutine(PickNewBackground());
+    }
+
+    private IEnumerator PickNewBackground() {
+        FileBrowser.SetFilters( true, new FileBrowser.Filter( "Background", ".png", ".jpg"));
+
+		FileBrowser.SetDefaultFilter( ".png" );
+
+		FileBrowser.AddQuickLink( "Users", "C:\\Users", null );
+
+        yield return FileBrowser.WaitForLoadDialog( FileBrowser.PickMode.Files, false, null, null, "Select background", "Load" );
+        Debug.Log( FileBrowser.Success );
+
+        if( FileBrowser.Success )
+			OnBackgroundSelected( FileBrowser.Result ); 
+    }
+
+    private void OnBackgroundSelected(string[] result) {
+        if (result.Length == 0) {
+            errorManager.DisplayError("ERROR: No file selected", 5);
+            return;
+        }
+
+        var backgroundFilePath = result[0];
+
+        try {
+            byte[] backgroundData = File.ReadAllBytes(backgroundFilePath);
+            var texture = new Texture2D(1, 1);
+            
+            if (texture.LoadImage(backgroundData)) {
+                texture.wrapMode = TextureWrapMode.Repeat;
+
+                skybox.SetTexture("_MainTex", texture);
+                RenderSettings.skybox = skybox;
+
+                DynamicGI.UpdateEnvironment();
+            } else {
+                errorManager.DisplayError("ERROR: Unable to load background", 5);
+            }
+        } catch (Exception e) {
+            errorManager.DisplayError("Error: " + e.Message, 5);
+        }
     }
 }
