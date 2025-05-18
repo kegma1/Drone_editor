cbbec13d89019f9794f3f4785cf3f7b250d910c0
Kennet Mathisen <kennetam02@gmail.com>
2025-05-19 01:22:24 +0200

    editing er done

--- Assets/Drones/VirtualDrone.cs
+++ Assets/Drones/VirtualDrone.cs
@@ -11,13 +11,15 @@ public class VirtualDrone
         this.color = color;
     }
 
-    public Vector3 ApplyTransformation(Transform transform, Rect sceneViewport, float scale, bool flipH, bool flipV) {
+    // Regner ut world-space posisjonen for en drone.
+    public Vector3 ApplyTransformation(Transform transform, Rect sceneViewport, float scale, bool flipH, bool flipV)
+    {
         int IflipH = flipH ? -1 : 1;
         int IflipV = flipV ? -1 : 1;
-        
+
         Vector3 localPos = new(this.pos.x * IflipV, this.pos.y * IflipH, 0);
-        localPos.x -= (sceneViewport.width/2) * scale;
-        localPos.y += (sceneViewport.height/2) * scale;
+        localPos.x -= (sceneViewport.width / 2) * scale;
+        localPos.y += (sceneViewport.height / 2) * scale;
 
         Vector3 rotatedPos = transform.rotation * localPos;
 
